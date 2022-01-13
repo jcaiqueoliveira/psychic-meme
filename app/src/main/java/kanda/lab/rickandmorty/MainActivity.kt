f72329c0b85c2dabca36fe88ce58a82d0661e038
jcaiqueoliveira <jcosilva@stone.com.br>
2022-01-12 22:24:38 -0300

    improve list style

--- app/src/main/java/kanda/lab/rickandmorty/MainActivity.kt
+++ app/src/main/java/kanda/lab/rickandmorty/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -13,6 +15,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
@@ -53,7 +56,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        modifier = Modifier.clickable {  }.padding(8.dp),
+        text = "Hello $name!"
+    )
 }
 
 @Preview(showBackground = true)
