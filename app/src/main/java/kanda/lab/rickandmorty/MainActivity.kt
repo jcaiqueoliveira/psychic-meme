32fa3bca324d2fb1a69c73e32cd0b0fc53f25cbe
Caique Oliveira <jcaique.jc@gmail.com>
2022-06-25 11:39:23 -0300

    improve home layout (#1)
    * improve home layout
    
    * add alpha for background

--- app/src/main/java/kanda/lab/rickandmorty/MainActivity.kt
+++ app/src/main/java/kanda/lab/rickandmorty/MainActivity.kt
@@ -4,24 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -39,23 +45,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         vm.characters()
         setContent {
-            RickandmortyTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Characters(vm)
-                }
-            }
+            Characters(vm)
         }
     }
 
+    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     private fun Characters(model: CharactersViewModel) {
         val characters by model.uiState.collectAsState()
-
-        LazyColumn(
-            contentPadding = PaddingValues(16.dp),
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(3),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            items(characters) { character ->
-                CharacterItem(character = character)
+            items(items = characters) {
+                CharacterItem(character = it)
             }
         }
     }
@@ -63,21 +67,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun CharacterItem(character: Character) {
-    Row(
+    Box(
+        contentAlignment = Alignment.BottomCenter,
         modifier = Modifier
-            .clickable { }
-            .fillMaxWidth()
-            .padding(8.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .clickable { },
     ) {
         Image(
+            modifier = Modifier.size(130.dp),
             painter = rememberImagePainter(character.image),
-            contentDescription = "Image from character named as ${character.name}",
-            modifier = Modifier.size(50.dp)
+            contentDescription = "Image from character ${character.name}",
         )
-        Spacer(modifier = Modifier.width(8.dp))
         Text(
-            modifier = Modifier.padding(8.dp),
-            text = character.name
+            text = character.name,
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Black.copy(alpha = 0.5f))
         )
     }
 }
